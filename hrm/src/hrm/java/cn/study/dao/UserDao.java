653eb0a9811a33eeafb553d95f3533edf83f9b5b
姜红 <whatwhywhen123@163.com>
2017-06-26 11:36:23 +0800

    1.applicationContext.xml：配置SqlSessionFactoryBean被spring管理。 2.userDao：依赖注入sqlSessionFactory 3.userDao：修改获取SqlSession对象的方法 4.Database：该类实现传统的通过mybatis-config.xml配置文件生成SqlSessionFactory对象，打开SqlSession，去掉该类。

--- hrm/src/hrm/java/cn/study/dao/UserDao.java
+++ hrm/src/hrm/java/cn/study/dao/UserDao.java
@@ -3,18 +3,22 @@ package cn.study.dao;
 import cn.study.domain.User;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserDao extends DatabaseSession implements IUserDao {
+public class UserDao implements IUserDao {
     //    对PO的操作，必须在SqlSession的管理下，才能够同步到数据库。即SqlSession对象是mybatis执行数据持久化的关键对象
     private SqlSession session;
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
 
     public void save(){
         //在DatabaseSession类中通过getSqlSession方法来获取数据库session，需要UserRepository类继承DatabaseSession类
         try{
-            this.session = super.getSqlSession();
+            this.session = sqlSessionFactory.openSession();
             User user = new User("hello","123");
             user.setLoginname("hello");
             this.session.insert("cn.study.mapper.UserMapper.save",user);
@@ -29,7 +33,7 @@ public class UserDao extends DatabaseSession implements IUserDao {
 
     public void findByUsername(){
         try{
-            this.session = super.getSqlSession();
+            this.session = sqlSessionFactory.openSession();
             User user = session.selectOne("cn.study.mapper.UserMapper.select_username","hello");
             System.out.println(user);
         }catch(Exception e){
